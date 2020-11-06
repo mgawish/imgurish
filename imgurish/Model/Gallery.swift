3cee78ae94a04e74754614187f4ed5a8139e64e9
Gawish <mohd.gawish@gmail.com>
2020-11-06 11:13:02 +0400

    added models again

---
+++ imgurish/Model/Gallery.swift
@@ -0,0 +1,12 @@
+//
+//  Gallery.swift
+//  imgurish
+//
+//  Created by Gawish on 06/11/2020.
+//
+
+import Foundation
+
+struct Gallery: Codable {
+    let data: [Post]
+}
---
+++ imgurish/Model/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  imgurish
+//
+//  Created by Gawish on 06/11/2020.
+//
+
+import Foundation
+
+struct Post: Codable, Identifiable {
+    let id: String
+    let title: String
+}
